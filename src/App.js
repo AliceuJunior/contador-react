50ce3c0adbb23a0d89083621118fc29d450e886b
AliceuJunior <adfj.junior@gmail.com>
2024-09-10 20:04:30 -0300

    Exercicio de Revisão da matéria de Laboratório de Desenvolvimento Web

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+const increment = () => setCount(count + 1);
+const decrement = () => setCount(count - 1);
+const reset = () => setCount(0);
+
+  return (
+    <div className="App">
+      <h1>Contador React</h1>
+      <p>Valor Atual: {count}</p>
+      <button onClick={increment}> Incrementar</button>
+      <button onClick={decrement}> Decremetar</button>
+      <button onClick={reset}> Resetar</button>
+    </div>
+  )
+}
+
+export default App;
